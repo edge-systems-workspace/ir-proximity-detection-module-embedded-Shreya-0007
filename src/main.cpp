a478939ac43a6804a168c29d2f7607229a56ad1b
Shreya-0007 <ss0684397@gmail.com>
2026-02-28 23:07:01 +0530

    Initialize Serial communication at 9600 baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -14,3 +14,12 @@
 
 #define IR_PIN 2
 int sensorState = 0;
+void setup() {
+
+    // TODO 3:
+    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
+
+    // TODO 4:
+    // Configure IR pin as INPUT
+    pinMode(IR_PIN, INPUT);
